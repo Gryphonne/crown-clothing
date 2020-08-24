f12daaf9778fb4b394744a6ecb581156815bc608
Gryphy <hippogryphy@hotmail.com>
2020-08-24 13:02:22 +0200

    updated shopdata with new data, updated cart utils to group by quantity in cart reducer

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,8 @@
 const CartActionTypes = {
-    TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN"
+    TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
+    ADD_ITEM: "ADD_ITEM",
+    REMOVE_ITEM: "REMOVE_ITEM",
+    CLEAR_ITEMS: "CLEAR_ITEMS"
 }
 
 export default CartActionTypes;
